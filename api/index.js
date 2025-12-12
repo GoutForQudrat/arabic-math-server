98c0c36e84a810e47875e87c301b1f696f329e85
GoutForQudrat <GOUTFORQUDRAT@GMAIL.COM>
2025-12-12 16:26:58 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,28 +1,37 @@
-const mjAPI = require("mathjax-node");
+const {mathjax} = require('mathjax-full/js/mathjax.js');
+const {TeX} = require('mathjax-full/js/input/tex.js');
+const {SVG} = require('mathjax-full/js/output/svg.js');
+const {liteAdaptor} = require('mathjax-full/js/adaptors/liteAdaptor.js');
+const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html.js');
+const {AllPackages} = require('mathjax-full/js/input/tex/AllPackages.js');
 
-// تشغيل المحرك بوضع أساسي سريع
-mjAPI.config({
-  MathJax: {
-    // نلغي الإضافات الخارجية عشان ما يعلق
-  }
-});
-mjAPI.start();
+// تجهيز المحرك (يشتغل مرة واحدة بس عشان السرعة)
+const adaptor = liteAdaptor();
+RegisterHTMLHandler(adaptor);
+
+const tex = new TeX({packages: AllPackages});
+const svg = new SVG({fontCache: 'none'});
+const html = mathjax.document('', {InputJax: tex, OutputJax: svg});
 
 module.exports = (req, res) => {
-  const latex = req.query.latex || "x";
-
-  mjAPI.typeset({
-    math: latex,
-    format: "TeX",
-    svg: true,
-  }, function (data) {
-    if (!data.errors) {
-      res.setHeader("Content-Type", "image/svg+xml");
-      // هذا الكود يخلي المتصفح يحفظ الصورة عشان تفتح بسرعة البرق مستقبلاً
-      res.setHeader("Cache-Control", "public, max-age=86400");
-      res.send(data.svg);
-    } else {
-      res.status(500).send("Error");
-    }
-  });
+  try {
+    const latex = req.query.latex || 'x';
+    
+    // عملية التحويل
+    const node = html.convert(latex, {
+      display: true,
+      em: 16,
+      ex: 8,
+      containerWidth: 80 * 16
+    });
+    
+    const svgString = adaptor.innerHTML(node);
+    
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate");
+    res.send(svgString);
+    
+  } catch (error) {
+    res.status(500).send("خطأ: " + error.message);
+  }
 };
