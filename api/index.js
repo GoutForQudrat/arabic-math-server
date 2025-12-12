f20387c132865fb80518fdc6338fd66db690a0b0
GoutForQudrat <GOUTFORQUDRAT@GMAIL.COM>
2025-12-12 16:37:57 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,19 +5,32 @@ const {liteAdaptor} = require('mathjax-full/js/adaptors/liteAdaptor.js');
 const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html.js');
 const {AllPackages} = require('mathjax-full/js/input/tex/AllPackages.js');
 
-// تجهيز المحرك (يشتغل مرة واحدة بس عشان السرعة)
 const adaptor = liteAdaptor();
 RegisterHTMLHandler(adaptor);
 
-const tex = new TeX({packages: AllPackages});
+// إعدادات المترجم العربي
+const tex = new TeX({
+  packages: AllPackages,
+  macros: {
+    // هنا نعرف أمر جديد اسمه "جذر عربي" يقوم بقلب الجذر ومحتواه مرتين عشان يضبط
+    arsqrt: ['\\style{transform: scaleX(-1); display:inline-block;}{\\sqrt{\\style{transform: scaleX(-1); display:inline-block;}{#1}}}', 1]
+  }
+});
+
 const svg = new SVG({fontCache: 'none'});
 const html = mathjax.document('', {InputJax: tex, OutputJax: svg});
 
 module.exports = (req, res) => {
   try {
-    const latex = req.query.latex || 'x';
+    let latex = req.query.latex || '1+2';
     
-    // عملية التحويل
+    // 1. تحويل الأرقام الإنجليزية إلى هندية (عربية) تلقائياً
+    const arabicDigits = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+    latex = latex.replace(/[0-9]/g, function(w) { return arabicDigits[+w] });
+
+    // 2. استبدال أي جذر عادي "sqrt" بجذرنا العربي المطور "arsqrt"
+    latex = latex.replace(/\\sqrt/g, '\\arsqrt');
+
     const node = html.convert(latex, {
       display: true,
       em: 16,
@@ -32,6 +45,6 @@ module.exports = (req, res) => {
     res.send(svgString);
     
   } catch (error) {
-    res.status(500).send("خطأ: " + error.message);
+    res.status(500).send("Error: " + error.message);
   }
 };
